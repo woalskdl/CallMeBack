4d976e72997efc835aaa66228c8cb0874e5ce4bf
wooljam <woalskdl@gmail.com>
2021-03-30 22:22:29 +0900

    1st 구조설계 - 매핑오류 해결

--- CallMeBack/src/main/java/com/jay/web/controller/MainController.java
+++ CallMeBack/src/main/java/com/jay/web/controller/MainController.java
@@ -2,13 +2,19 @@ package com.jay.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 	
-	@RequestMapping(value="/main")
-	public String main() {
-		return "customer/main";
+	@RequestMapping("main")
+	public void main() {
+		
+		System.out.println("실행됨");
+//		ModelAndView mv = new ModelAndView("customer/main");
+		
+//		return mv;
 	}
 }
